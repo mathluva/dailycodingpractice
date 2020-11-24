e304bd2c054241ccc5662bf7ea2009b64704368c
mathluva <mathluvacoding@gmail.com>
2020-11-24 16:16:13 -0500

    11/25 practice

--- coding.js
+++ coding.js
@@ -28,25 +28,62 @@
 // If a[i] > b[i], then Alice is awarded 1 point.
 // If a[i] < b[i], then Bob is awarded 1 point.
 // If a[i] = b[i], then neither person receives a point.
-var final =[0,0];
-arr = [1, 2, 3];
-brr = [3, 2, 1];
-function score(a,b){
-    for(var i=0;i<a.length;i++);
-    if (a[i]>b[i]){
-        final[0] = final[0] +1;}
+// var final =[0,0];
+// arr = [1, 2, 3];
+// brr = [3, 2, 1];
+// function score(a,b){
+//     for(var i=0;i<a.length;i++);
+//     if (a[i]>b[i]){
+//         final[0] = final[0] +1;}
 
-    else if (a[i]<b[i]){
-        final[1] = final[1] +1;}
-    else (a[i]=b[i]);{
-        final[0] = final[0] +0;
-        final[1] = final[1] +0;
-    }
-    return final;
-    console.log(final);
+//     else if (a[i]<b[i]){
+//         final[1] = final[1] +1;}
+//     else (a[i]=b[i]);{
+//         final[0] = final[0] +0;
+//         final[1] = final[1] +0;
+//     }
+//     return final;
+//     console.log(final);
    
+// };
+
+
+// score(arr,brr);
+// console.log("h","k");
+
+
+// function twoSum(nums, target) {
+//     for(var i=0; i<nums.length;i++){
+//         for(var j= i+1; j<nums.length; j++)
+//             if (nums[i]+nums[j]==target){
+//                 console.log(i,j);
+//             }
+//             else{
+//             console.log("-1,-1");
+//             }
+//     }
+// };
+// numbers=[3,4,6,0,10]
+// value= 10
+// twoSum(numbers,value);
+
+var pairs =0;
+function sockMerchant(ar) {
+    for(var i =0; i<ar.length; i++){
+        for(var j = i+1; j<ar.length-1; j++){
+            if(ar[i]===ar[j]){
+                pairs = pairs +1;
+                // console.log(i,j);
+                ar.splice(i,1);
+            }
+        }
+    }
+    console.log(pairs);
+    console.log(ar);
 };
 
 
-score(arr,brr);
-console.log("h","k");
+// var number_socks = 10
+sock_color = [1,1,2,2,3,5]
+// sock_color = [1,4,3,5,4,1,3,5,1,3]
+sockMerchant(sock_color);
