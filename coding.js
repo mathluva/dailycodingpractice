124bc8672a974151f43391885b44be897a2392a4
mathluva <mathluvacoding@gmail.com>
2021-01-30 01:27:35 -0500

    daily practice

--- coding.js
+++ coding.js
@@ -159,26 +159,67 @@
 
 
 
-/* Hackerrank Cloud*/
-clouds = [0, 0, 0, 0, 1, 0]
-clouds2 = [0, 0, 1, 0, 0, 1, 0]
-function jumpingOnClouds(c) {
-    let jumps = 0;
-    for(let i = 0; i<c.length-1;){
-          if( c[i]==c[i+1] && c[i+2]==0){
-              jumps +=1;
-              i = i+2;
-          }
-          else if(c[i]==c[i+1] && c[i+2]==1){
-              jumps +=1;
-              i = i+1;
-          }
-          else{
-              jumps +=1;
-              i = i+2;
-          }
+// /* Hackerrank Cloud*/
+// clouds = [0, 0, 0, 0, 1, 0]
+// clouds2 = [0, 0, 1, 0, 0, 1, 0]
+// function jumpingOnClouds(c) {
+//     let jumps = 0;
+//     for(let i = 0; i<c.length-1;){
+//           if( c[i]==c[i+1] && c[i+2]==0){
+//               jumps +=1;
+//               i = i+2;
+//           }
+//           else if(c[i]==c[i+1] && c[i+2]==1){
+//               jumps +=1;
+//               i = i+1;
+//           }
+//           else{
+//               jumps +=1;
+//               i = i+2;
+//           }
+//         }
+//     return jumps;
+// }
+// // console.log(jumpingOnClouds(clouds));
+// // console.log(jumpingOnClouds(clouds2));
+
+// // string1 = 'aedbdaeaddadddcedcbbabdccbecaecaccdbebeeadadcaabbaabbaeeeecaddbcdecbbdccdebaaebecdaaabbcdeccbabaabce' //abaabaabaabaabaabaabaabaabaaba
+//     // function repeatedString(s, n) {
+//     //     let count = 0;
+//     //     for (var i = 0; i < s.length; i++){
+//     //         if(s[i]== 'a'){
+//     //         count +=1;
+//     //     }
+//     // } 
+//     //     newstr = ;
+//     //     return value;
+//     // }
+//     // console.log(repeatedString(string1,731869010806));//168329872486
+// //     let string1 = 'abc'
+// // console.log(string1.repeat(2))
+
+say =  [1, 2, 3, 4, 5]
+
+// function sort(nums) {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i+1; j < nums.length; j++){
+//               if(nums[i] > nums[j]){
+//                   [nums[i], nums[j]] = [nums[j], nums[i]]; //swap position
+//               }
+//         }
+//      }
+//     return nums;
+// };
+
+
+function sort(nums) {
+    for(let i = 0; i < nums.length; i--){
+        for(let j = i+1; j < nums.length; j--){
+              if(nums[i] > nums[j]){
+                  [nums[i], nums[j]] = [nums[j], nums[i]]; //swap position
+              }
         }
-    return jumps;
-}
-console.log(jumpingOnClouds(clouds));
-console.log(jumpingOnClouds(clouds2));
+     }
+    return nums;
+};
+console.log(sort(say));
